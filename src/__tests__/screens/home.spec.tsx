573f989f51008019324de5597cd87fca82bf8811
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-02-17 21:33:52 -0300

    chore: jest-expo added

--- src/__tests__/screens/home.spec.tsx
+++ src/__tests__/screens/home.spec.tsx
@@ -1,1 +1,10 @@
 import React from "react";
+import { render } from "@testing-library/react-native";
+
+import Home from "../../screens/Home";
+
+it("should text input with 'Name' placeholder exists", () => {
+  const { debug } = render(<Home />);
+
+  debug();
+});
